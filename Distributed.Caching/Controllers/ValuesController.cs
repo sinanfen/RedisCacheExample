f10e36e91b82e502e6f1bfc353fb95026223ad4a
Sinan Fen <sinanfen@hotmail.com>
2024-07-29 02:43:47 +0300

    redis caching

--- Distributed.Caching/Controllers/ValuesController.cs
+++ Distributed.Caching/Controllers/ValuesController.cs
@@ -17,24 +17,61 @@ public class ValuesController : ControllerBase
         _distributedCache = distributedCache;
     }
 
+
     [HttpGet("set")]
     public async Task<IActionResult> Set(string name, string surname)
     {
-        await _distributedCache.SetStringAsync("name", name);
-        await _distributedCache.SetAsync("surname", Encoding.UTF8.GetBytes(surname));
-        return Ok();
+        try
+        {
+            // Check and remove existing keys to avoid WRONGTYPE error
+            if (await _distributedCache.GetAsync("name") != null)
+                await _distributedCache.RemoveAsync("name");
+
+
+            if (await _distributedCache.GetAsync("surname") != null)
+                await _distributedCache.RemoveAsync("surname");
+
+            // Set new values
+            await _distributedCache.SetStringAsync("name", name, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+            await _distributedCache.SetAsync("surname", Encoding.UTF8.GetBytes(surname), options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            // Log the exception (for debugging purposes)
+            Console.WriteLine($"Error: {ex.Message}");
+            return StatusCode(500, "Internal server error");
+        }
     }
 
     [HttpGet("get")]
     public async Task<IActionResult> Get()
     {
-        var name = await _distributedCache.GetStringAsync("name");
-        var surnameBinary = await _distributedCache.GetAsync("surname");
-        var surname = Encoding.UTF8.GetString(surnameBinary);
-        return Ok(new
+        try
         {
-            name,
-            surname
-        });
+            var name = await _distributedCache.GetStringAsync("name");
+            var surname = await _distributedCache.GetStringAsync("surname");
+
+            return Ok(new
+            {
+                name,
+                surname
+            });
+        }
+        catch (Exception ex)
+        {
+            // Log the exception (for debugging purposes)
+            Console.WriteLine($"Error: {ex.Message}");
+            return StatusCode(500, "Internal server error");
+        }
     }
 }
