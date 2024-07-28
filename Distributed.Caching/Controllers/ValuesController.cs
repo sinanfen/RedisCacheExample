91b43cf8c5f928eed7dd3e6c7b9010195e61bde3
Sinan Fen <sinanfen@hotmail.com>
2024-07-29 02:33:48 +0300

    Add project files.

---
+++ Distributed.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Distributed.Caching.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    private readonly IDistributedCache _distributedCache;
+
+    public ValuesController(IDistributedCache distributedCache)
+    {
+        _distributedCache = distributedCache;
+    }
+
+    [HttpGet("set")]
+    public async Task<IActionResult> Set(string name, string surname)
+    {
+        await _distributedCache.SetStringAsync("name", name);
+        await _distributedCache.SetAsync("surname", Encoding.UTF8.GetBytes(surname));
+        return Ok();
+    }
+
+    [HttpGet("get")]
+    public async Task<IActionResult> Get()
+    {
+        var name = await _distributedCache.GetStringAsync("name");
+        var surnameBinary = await _distributedCache.GetAsync("surname");
+        var surname = Encoding.UTF8.GetString(surnameBinary);
+        return Ok(new
+        {
+            name,
+            surname
+        });
+    }
+}
---
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    private readonly IMemoryCache _memoryCache;
+
+    public ValuesController(IMemoryCache memoryCache)
+    {
+        this._memoryCache = memoryCache;
+    }
+
+    [HttpGet("set/{name}")]
+    public void SetName(string name)
+    {
+        _memoryCache.Set("name", name);
+    }
+
+    [HttpGet]
+    public string GetName()
+    {
+        if (_memoryCache.TryGetValue<string>("name", out string name))
+            return name;
+        return "";
+    }
+
+    [HttpGet("setDate")]
+    public void SetDate()
+    {
+        _memoryCache.Set<DateTime>("date", DateTime.UtcNow, options: new()
+        {
+            AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+            SlidingExpiration = TimeSpan.FromSeconds(5),
+        });
+    }
+
+    [HttpGet("getDate")]
+    public DateTime GetDate()
+    {
+        return _memoryCache.Get<DateTime>("date");
+    }
+}
