e34fb5c7c7fb2f327a84a7cb7dbecd081d10eef7
Sinan Fen <sinanfen@hotmail.com>
2024-08-11 19:52:22 +0300

    Redis Pub/Sub - publish - subscribe operations.

---
+++ Redis.Pub.Sub.Example/Program.cs
@@ -0,0 +1,13 @@
+﻿using StackExchange.Redis;
+
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1401");
+
+ISubscriber subscriber = connection.GetSubscriber();
+
+while (true)
+{
+    Console.Write("Mesaj : ");
+    string message = Console.ReadLine();
+    var clients = await subscriber.PublishAsync("mychannel",message);
+}
---
+++ Redis.Pub.Sub.Subscriber.Example/Program.cs
@@ -0,0 +1,13 @@
+﻿using StackExchange.Redis;
+
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1401");
+
+ISubscriber subscriber = connection.GetSubscriber();
+
+await subscriber.SubscribeAsync("mychannel.*", (channel, message) =>
+{
+    Console.Write(message);
+});
+
+Console.Read();
